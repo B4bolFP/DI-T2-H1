49426ef0153a4c7e151553e624bf09beeb31f372
Mario Lopez <mario.lopezl@campusfp.es>
2024-01-29 20:12:43 +0100

    Hola mundo en Angular

---
+++ angular-hola-mundo/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {HolaMundoComponent} from './hola-mundo/hola-mundo.component'
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HolaMundoComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = 'angular-hola-mundo';
+}
